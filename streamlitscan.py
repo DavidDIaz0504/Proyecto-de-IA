822155fbc87c24f8242f81e589bdc45cbd922207
DavidDIaz0504 <deyvidsoz770@gmail.com>
2024-05-28 13:27:04 -0500

    ver app streamlit and apk mobile

---
+++ streamlitscan.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+# Cargar el modelo
+model = tf.keras.models.load_model('Model.h5')
+
+# Función para predecir la clase de una imagen
+def predict(image):
+    img_array = np.array(image.resize((224, 224))) / 255.0  # Ajustar el tamaño de la imagen según sea necesario
+    img_array = np.expand_dims(img_array, axis=0)
+    predictions = model.predict(img_array)
+    class_idx = np.argmax(predictions)
+    confidence = np.max(predictions)
+    return class_idx, confidence
+
+st.title('Product Scanner')
+
+# Inicializar la cámara
+camera = cv2.VideoCapture(0)
+
+# Crear una variable para almacenar la última imagen capturada
+last_frame = None
+
+while True:
+    # Capturar un fotograma de la cámara
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Mostrar el fotograma en la interfaz de Streamlit
+    st.image(frame, channels="RGB", use_column_width=True)
+    
+    # Guardar el fotograma actual
+    last_frame = frame
+    
+    # Si se presiona el botón de captura, detener el bucle
+    if st.button('Capture'):
+        break
+
+# Convertir la última imagen capturada en un objeto de imagen de PIL
+captured_image = Image.fromarray(last_frame)
+
+# Predecir la clase de la imagen capturada
+class_idx, confidence = predict(captured_image)
+
+# Mostrar la clasificación y la confianza
+st.write(f'Class: {class_idx}, Confidence: {confidence:.2f}')
+
+# Liberar la cámara
+camera.release()
+cv2.destroyAllWindows()
---
+++ versions.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import streamlit as st
+import cv2
+from PIL import Image
+
+print("TensorFlow version:", tf.__version__)
+print("Streamlit version:", st.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Pillow version:", Image.__version__)
